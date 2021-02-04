a77851184dacf8ba502534193fd80a3740c34d1c
carlosvicient <carlos.vicient@ntnu.no>
2021-02-04 13:12:49 +0100

    test fix build error

--- src/lambda/slack.js
+++ src/lambda/slack.js
@@ -1,4 +1,6 @@
-const fetch = require("node-fetch");
+//const fetch = require("node-fetch");
+import fetch from 'node-fetch';
+
 const slackURL = process.env.SLACK_WEBHOOK_URL;
 
 export function handler(event, context, callback) {
